ace40f91725833d582639e663da5e82372d18c5a
coderyyx <358231061@qq.com>
2016-09-28 15:39:19 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,8 @@ module.exports = {
   module: {
     loaders: [
       { 
-        test: /\.jsx?$/,loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {presets: ['es2015', 'react']}
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
       },
       {
         test:/\.css$/,
